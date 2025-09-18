26eb24e80d6a1bca7f9739b47f019ad78488a477
krp <Priya.KR@moodys.com>
2025-09-18 22:30:27 +0530

    Updated Test-Report configs

--- map-auth-service-qa/playwright.config.ts
+++ map-auth-service-qa/playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-
+ 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -7,12 +7,12 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
-
+ 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './tests/sso-dashboard/api-automation/',
   timeout: 120000,
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -24,48 +24,52 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
+  reporter: [
+  ['html', { outputFolder: 'playwright-report/html-report', open: 'never' }],
+  ['json', { outputFile: 'playwright-report/test-results/api-test-results.json' }]
+],
+ 
+ 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    
+   
     //it will turn on the video and execution speed can be configured from here.
     // video: 'on',
     // launchOptions:
     // {
     //   slowMo:1000
     // },
-
+ 
     // only for first retry.
     //video: 'on-first-retry',
     //only for failed cases
     //video: 'retain-on-failure',
-    
+   
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
+ 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
+   
   },
-
+ 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+ 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+ 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+ 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -75,7 +79,7 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
-
+ 
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
@@ -86,7 +90,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+ 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
